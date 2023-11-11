40d5c13ea7a15c6f38b12a620ed3dec75e207bca
Mark Butler <mrkcbutler@gmail.com>
2023-11-11 11:31:41 -0600

    Add function to check table existence in database

--- notebook_tools/database.py
+++ notebook_tools/database.py
@@ -11,6 +11,13 @@ from notebook_tools import DATA_DIR
 DATABASE_PATH = DATA_DIR / "lending-club.sqlite"
 DATABASE_ENGINE = create_engine(url="sqlite:///" + str(DATABASE_PATH))
 
+# Comments to ignore type checking are needed in a few places in this module because
+# DATABASE_ENGINE is temporarily set to None by the function _delete_database.  The
+# function _raise_for_missing_engine raises an exception if DATABASE_ENGINE is None, and
+# this function is executed before each call to DATABASE_ENGINE.connect(). All
+# type-checking errors disappear if each call to _raise_for_missing_engine is
+# replaced with the code executed by the function, but this is overly verbose.
+
 SQL_STRINGS = {
     "loan_data": """
         SELECT *
@@ -18,6 +25,11 @@ SQL_STRINGS = {
     "loan_metadata": """
         SELECT *
         FROM loan_metadata;""",
+    "table_exists": """
+        SELECT EXISTS (SELECT name
+                       FROM sqlite_master
+                       WHERE type = 'table'
+                         AND name = :table_name) AS table_exists;""",
 }
 
 
@@ -56,11 +68,8 @@ def _perform_query(query, params=None):
     Returns:
         Dataframe corresponding to the query result
     """
-    if DATABASE_ENGINE is None:
-        raise RuntimeError(
-            "Unable to perform database query because the engine is not instantiated."
-        )
-    with DATABASE_ENGINE.connect() as con:
+    _raise_for_missing_engine()
+    with DATABASE_ENGINE.connect() as con:  # type: ignore
         return read_sql_query(query, con, params=params)
 
 
@@ -107,11 +116,32 @@ def add_tables(tables, **kwargs):
             that add_tables passes index=False to to_sql, so 'index' should not be used
             as one of the keyword arguments.
     """
+    _raise_for_missing_engine()
+    with DATABASE_ENGINE.connect() as con:  # type: ignore
+        for name, df in tables.items():
+            df.to_sql(name, con=con, index=False, **kwargs)
+        con.commit()
+
+
+def _raise_for_missing_engine():
     if DATABASE_ENGINE is None:
         raise RuntimeError(
             "Unable to add database tables because the engine is not instantiated."
         )
-    with DATABASE_ENGINE.connect() as con:
-        for name, df in tables.items():
-            df.to_sql(name, con=con, index=False, **kwargs)
-        con.commit()
+
+
+def table_exists(table_name):
+    """Check whether a table exists in the project database.
+
+    Args:
+        table_name:  The name of the table to check for
+
+    Return:
+        Boolean indicating whether the table exists
+    """
+    _raise_for_missing_engine()
+    with DATABASE_ENGINE.connect() as con:  # type: ignore
+        result = con.execute(
+            text(SQL_STRINGS["table_exists"]), {"table_name": table_name}
+        )
+        return bool(result.first().table_exists)  # type: ignore
